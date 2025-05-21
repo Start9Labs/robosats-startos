edfe9a52c0acfabd5293bbef0ab0785c0ba19001
Matt Hill <mattnine@protonmail.com>
2025-05-21 15:17:16 -0600

    beta 23

---
+++ startos/install/versions/index.ts
@@ -0,0 +1,2 @@
+export { v_0_7_7_1 as current } from './v0.7.7.1'
+export const other = []
--- startos/versions/index.ts
+++
@@ -1,4 +0,0 @@
-import { VersionGraph } from '@start9labs/start-sdk'
-import { v_0_7_7_1 } from './v0.7.7.1'
-
-export const versions = VersionGraph.of(v_0_7_7_1)
