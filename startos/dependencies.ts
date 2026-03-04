4da94f9c21b5ebe9312a234e5f90b585917d8575
Matt Hill <mattnine@protonmail.com>
2026-03-04 09:40:43 -0700

    newer tor

--- startos/dependencies.ts
+++ startos/dependencies.ts
@@ -1,11 +1,9 @@
 import { sdk } from './sdk'
 
-export const setDependencies = sdk.setupDependencies(
-  async ({ effects }) => ({
-    tor: {
-      kind: 'running',
-      versionRange: '>=0.4.5:0',
-      healthChecks: ['tor'],
-    },
-  }),
-)
+export const setDependencies = sdk.setupDependencies(async ({ effects }) => ({
+  tor: {
+    kind: 'running',
+    versionRange: '>=0.4.8:0-beta.0',
+    healthChecks: ['tor'],
+  },
+}))
